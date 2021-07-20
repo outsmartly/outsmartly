a8614f17764001770c13efe142dbe2468bec0be1
Jay Phelps <hello@jayphelps.com>
2021-07-20 15:00:27 -0500

    message-bus: add tests for messageBus.on()

--- packages/message-bus/src/__tests__/MessageBus.spec.ts
+++ packages/message-bus/src/__tests__/MessageBus.spec.ts
@@ -3,7 +3,7 @@ import { MessageBus } from '../MessageBus';
 describe('MessageBus', () => {
   const throttleDelay = 100;
   class TestMessageBus extends MessageBus {
-    _throttleDelay = throttleDelay;
+    override _throttleDelay = throttleDelay;
     _writeToExternal = jest.fn();
     _waitUntil = jest.fn();
   }
@@ -51,36 +51,72 @@ describe('MessageBus', () => {
       expect(messages._writeToExternal).toBeCalledTimes(1);
       expect(messages._writeToExternal).toBeCalledWith(expectedMessages);
     });
+
+    it('writes messages to external explicitly flushed early', async () => {
+      const expectedMessages = [
+        {
+          type: 'Example.FIRST',
+          data: {
+            first: 1,
+          },
+        },
+        {
+          type: 'Example.SECOND',
+          data: {
+            second: 2,
+          },
+        },
+      ];
+      for (const { type, data } of expectedMessages) {
+        messages.emit(type, data);
+      }
+
+      expect(messages._waitUntil).toBeCalledTimes(1);
+      expect(messages._waitUntil).toBeCalledWith(Promise.resolve());
+
+      expect(messages._writeToExternal).toBeCalledTimes(0);
+      jest.advanceTimersByTime(throttleDelay / 2);
+      expect(messages._writeToExternal).toBeCalledTimes(0);
+      messages.flushToExternal();
+
+      expect(messages._writeToExternal).toBeCalledTimes(1);
+      expect(messages._writeToExternal).toBeCalledWith(expectedMessages);
+    });
   });
 
-  it('writes messages to external explicitly flushed early', async () => {
-    const expectedMessages = [
-      {
-        type: 'Example.FIRST',
-        data: {
-          first: 1,
-        },
-      },
-      {
-        type: 'Example.SECOND',
-        data: {
-          second: 2,
-        },
-      },
-    ];
-    for (const { type, data } of expectedMessages) {
+  describe('on()', () => {
+    it('does not invoke listener for previously (missed) messages', async () => {
+      const type = 'Example.FIRST';
+      const data = {
+        first: 1,
+      };
       messages.emit(type, data);
-    }
+      const listener = jest.fn();
+      messages.on(type, listener);
+      messages.flushToExternal();
+      expect(listener).toBeCalledTimes(0);
+    });
 
-    expect(messages._waitUntil).toBeCalledTimes(1);
-    expect(messages._waitUntil).toBeCalledWith(Promise.resolve());
+    it('does not invoke listener for non-matching message types', async () => {
+      const listener = jest.fn();
+      messages.on('Example.FIRST', listener);
+      messages.emit('Example.DOES_NOT_MATCH', null);
+      expect(listener).toBeCalledTimes(0);
+    });
 
-    expect(messages._writeToExternal).toBeCalledTimes(0);
-    jest.advanceTimersByTime(throttleDelay / 2);
-    expect(messages._writeToExternal).toBeCalledTimes(0);
-    messages.flushToExternal();
-
-    expect(messages._writeToExternal).toBeCalledTimes(1);
-    expect(messages._writeToExternal).toBeCalledWith(expectedMessages);
+    it('invokes listener for any future matching events', async () => {
+      const type = 'Example.FIRST';
+      const data = {
+        first: 1,
+      };
+      const listener = jest.fn();
+      messages.on(type, listener);
+      messages.emit(type, data);
+      expect(listener).toBeCalledTimes(1);
+      expect(listener).toBeCalledWith({
+        type,
+        data
+      });
+    });
   });
 });
