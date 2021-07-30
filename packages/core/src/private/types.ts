7c017058f78fa47a9d528f94a107cae481328c55
Jay Phelps <hello@jayphelps.com>
2021-07-30 16:44:13 -0500

    core: remove extraneous comments

--- packages/core/src/private/types.ts
+++ packages/core/src/private/types.ts
@@ -1,15 +1,11 @@
 import { MessageBus } from '../public/MessageBus';
 import { OutsmartlyClientVisitor, OutsmartlyVisitor } from '../public/types';
 
-// Internal implementation details, not publicly accessible.
-// export type Context = unknown;
-
 export interface Context {
   messageBus: MessageBus;
   visitor: OutsmartlyVisitor;
 }
 
-// Internal implementation details
 export interface ClientContext extends Context {
   visitor: OutsmartlyClientVisitor;
 }
