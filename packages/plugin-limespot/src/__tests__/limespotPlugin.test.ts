524209328102f2092d96de22f6621f683c45654e
Carlos Moreno <soyelmoreno@gmail.com>
2021-08-06 16:05:29 -0700

    Refactor to extract common functionality for each test

--- packages/plugin-limespot/src/__tests__/limespotPlugin.test.ts
+++ packages/plugin-limespot/src/__tests__/limespotPlugin.test.ts
@@ -36,28 +36,34 @@ describe('Limespot plugin', () => {
   type MySpy = jest.SpyInstance<number, []> & (() => number);
   let dateNowSpy: MySpy;
 
+  // Used in all the tests
+  const plugin = limespotPlugin();
+  const contextId = '888899991111'; // fake
+  const getUrl = (timestamp: number): string => {
+    return `https://storefront.personalizer.io/v1/activityLogs?batch=true&t=${timestamp}`;
+  };
+  const getOptions = (contextId: string, buffer: Object[]) => {
+    return {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'X-Personalizer-Context-ID': contextId,
+      },
+      body: JSON.stringify(buffer),
+    };
+  };
+
   beforeEach(() => {
     fetchMock.enableMocks();
     fetchMock.resetMocks();
     messageBus = new TestMessageBus() as any as EdgeMessageBus;
     dateNowSpy = jest.spyOn(Date, 'now').mockImplementation(() => 1337) as MySpy;
-  });
 
-  afterEach(() => {
-    dateNowSpy.mockRestore();
-  });
-
-  afterAll(() => {
-    fetchMock.mockRestore();
-  });
-
-  it.only('logs a "<Box>RecommendationsRendered" event when a "boxRender" event is emitted', (done) => {
-    expect.assertions(2);
-
-    const plugin = limespotPlugin();
+    // Used in all the tests, but depends on stuff inside beforeEach
     plugin.setup?.({ config: null as any, messageBus });
-    const contextId = '888899991111'; // fake
-
+    fetchMock.mockOnce('', {
+      status: 204,
+    });
     notifyListener.mockImplementationOnce(
       (
         listener: MessageBusListener<OutsmartlyMessageEvent<string, unknown>>,
@@ -74,13 +80,22 @@ describe('Limespot plugin', () => {
           },
         } as any;
         const event = new TestMessageEvent(messageBus, visitor, message, cookies);
-        Promise.resolve(listener(event)).catch(done);
+        // Promise.resolve(listener(event)).catch(done);
+        return Promise.resolve(listener(event));
       },
     );
+  });
 
-    fetchMock.mockOnce('', {
-      status: 204,
-    });
+  afterEach(() => {
+    dateNowSpy.mockRestore();
+  });
+
+  afterAll(() => {
+    fetchMock.mockRestore();
+  });
+
+  it('logs a "<Box>RecommendationsRendered" event when a "boxRender" event is emitted', (done) => {
+    expect.assertions(2);
 
     // Input
     const obj = {
@@ -101,55 +116,24 @@ describe('Limespot plugin', () => {
     };
     buffer.push(payload);
 
+    const url = getUrl(dateNowSpy());
+    const options = getOptions(contextId, buffer);
+
     // We need to get in line (i.e., the plugin batches events together and
     // calls fetch() from within a queueMicroTask(), so we need to use it also)
     queueMicrotask(() => {
-      // Fetch was called once from the plugin
+      // Fetch should be called once from the plugin
       expect(fetchMock).toBeCalledTimes(1);
 
-      // Fetch was called with this stuff
-      expect(fetchMock).toBeCalledWith(
-        `https://storefront.personalizer.io/v1/activityLogs?batch=true&t=${dateNowSpy()}`,
-        {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-            'X-Personalizer-Context-ID': contextId,
-          },
-          body: JSON.stringify(buffer),
-        },
-      );
+      // Fetch should be called with this stuff
+      expect(fetchMock).toBeCalledWith(url, options);
 
       done();
     });
   });
 
-  it.only('logs an "ItemView" event when a "productView" event is emitted', (done) => {
+  it('logs an "ItemView" event when a "productView" event is emitted', (done) => {
     expect.assertions(2);
-    const plugin = limespotPlugin();
-    plugin.setup?.({ config: null as any, messageBus });
-    const contextId = '888899991111';
-    notifyListener.mockImplementationOnce(
-      (
-        listener: MessageBusListener<OutsmartlyMessageEvent<string, unknown>>,
-        message: MessageBusMessage<string, unknown>,
-      ) => {
-        const visitor = {} as any;
-        const cookies = {
-          get(key: string): string {
-            if (key !== 'lsContextID') {
-              throw new Error(`Unexpected cookie key ${key}`);
-            }
-            return contextId;
-          },
-        } as any;
-        const event = new TestMessageEvent(messageBus, visitor, message, cookies);
-        Promise.resolve(listener(event)).catch(done);
-      },
-    );
-    fetchMock.mockOnce('', {
-      status: 204,
-    });
     const obj = {
       id: '444555666',
       resolution: '1980 x 1200',
@@ -164,19 +148,11 @@ describe('Limespot plugin', () => {
       ScreenResolution: obj.resolution,
     };
     buffer.push(payload);
+    const url = getUrl(dateNowSpy());
+    const options = getOptions(contextId, buffer);
     queueMicrotask(() => {
       expect(fetchMock).toBeCalledTimes(1);
-      expect(fetchMock).toBeCalledWith(
-        `https://storefront.personalizer.io/v1/activityLogs?batch=true&t=${dateNowSpy()}`,
-        {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-            'X-Personalizer-Context-ID': contextId,
-          },
-          body: JSON.stringify(buffer),
-        },
-      );
+      expect(fetchMock).toBeCalledWith(url, options);
       done();
     });
   });
